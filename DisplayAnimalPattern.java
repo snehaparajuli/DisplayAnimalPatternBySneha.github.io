f17268375a6a6c1090cb3ae3dfbea59196560896
snehaparajuli <sneha_0326@deerwalk.edu.np>
2015-01-26 13:54:25 +0545

    print ln function for animal pattern

---
+++ DisplayAnimalPattern.java
@@ -0,0 +1,16 @@
+/**
+ * Created by subha on 2015-01-19.
+ */
+
+public class DisplayAnimalPattern{
+
+    public static void main(String[] args){
+        System.out.println("\t\t  '__'");
+        System.out.println("\t\t  (\u00A9\u00A9)");
+        System.out.println("\t/========\\/");
+        System.out.println("\t /|| %% ||");
+        System.out.println("\t* ||----||");
+        System.out.println("\t  \u00A5\u00A5\t\u00A5\u00A5");
+        System.out.println("  \t  \"\"\t\"\" ");
+    }
+}
